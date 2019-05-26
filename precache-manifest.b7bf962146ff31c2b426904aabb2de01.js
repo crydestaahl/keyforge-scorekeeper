82c039275bf0fadf84f5b77569b9e3a87410c98d
crydestaahl <kristoffer@oholics.com>
2019-05-26 21:07:11 +0200

    Updates

---
+++ precache-manifest.b7bf962146ff31c2b426904aabb2de01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6695ee1720b6309fd8b823265faaccc9",
+    "url": "/keyforge-scorekeeper/index.html"
+  },
+  {
+    "revision": "ff4100c44b38f073be95",
+    "url": "/keyforge-scorekeeper/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ff4100c44b38f073be95",
+    "url": "/keyforge-scorekeeper/static/js/2.43d0d905.chunk.js"
+  },
+  {
+    "revision": "210ecee39529d936c7db",
+    "url": "/keyforge-scorekeeper/static/js/main.51511e7d.chunk.js"
+  },
+  {
+    "revision": "464805a6d31c1fa197c3",
+    "url": "/keyforge-scorekeeper/static/js/runtime~main.5dd31267.js"
+  },
+  {
+    "revision": "01bd39f20456a46622f797dbb1378a72",
+    "url": "/keyforge-scorekeeper/static/media/keyforge-bg.01bd39f2.jpg"
+  }
+]);
